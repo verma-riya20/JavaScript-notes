fbf395a8e3fb3a76b274d9283ef32f7050cac850
verma-riya20 <157815520+verma-riya20@users.noreply.github.com>
2024-01-26 23:25:08 +0530

    Create math.js

---
+++ math.js
@@ -0,0 +1,30 @@
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(Math.PI));
+console.log(Math.ceil(Math.PI));
+console.log(Math.floor(Math.PI));
+console.log(Math.trunc(Math.PI));
+console.log(Math.pow(4,2));
+console.log(Math.random());
+console.log(Math.max(4,2));
+//random no between 1 to 10
+console.log(Math.floor(Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+//first code
+//code to return a random number from your name
+const myString="riyaverma";
+console.log(myString.length)
+console.log(myString.charAt(Math.floor(Math.random()*myString.length)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));myString
+console.log(myString.charAt(Math.floor(Math.random()*9)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));
+console.log(myString.charAt(Math.floor(Math.random()*9)));
