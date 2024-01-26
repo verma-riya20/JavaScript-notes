056469a0bf12439342fb6abfad7835116a9c97aa
verma-riya20 <157815520+verma-riya20@users.noreply.github.com>
2024-01-26 23:21:54 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,3 +1,16 @@
 console.log("hello world");
 console.log(typeof "tom");
 console.log(typeof 45);
+
+let myVar="mathematics";
+console.log(myVar.length);
+console.log("verma".length);
+console.log(myVar.charAt(3));
+console.log(myVar.indexOf("at"));
+console.log(myVar.lastIndexOf("at")); 
+console.log(myVar.slice(5,9));
+console.log(myVar.toLowerCase());
+console.log(myVar.toUpperCase());
+console.log(myVar.includes("at"));
+console.log(myVar.split(""));
+
